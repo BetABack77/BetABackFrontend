c1c0c20bf69ee92dc192bde82ba97988e90772ca
BetABack <betabackuserhelp@gmail.com>
2025-05-30 15:06:06 +0530

    url reomve localhost 300

--- src/Services/axiosInstance.ts
+++ src/Services/axiosInstance.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.VITE_BASE_URL || "https://betabackbackend.onrender.com";
-export const WebSocket_URL =
-  import.meta.env.VITE_WEBSOCKET_URL || "https://betabackbackend.onrender.com";
+// const BASE_URL =
+//   import.meta.env.VITE_BASE_URL || "https://betabackbackend.onrender.com";
+// export const WebSocket_URL =
+//   import.meta.env.VITE_WEBSOCKET_URL || "https://betabackbackend.onrender.com";
 
-  
+const BASE_URL = "https://betabackbackend.onrender.com";
+export const WebSocket_URL = "https://betabackbackend.onrender.com";
+
 // Create an Axios instance
 const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api/v1`, // Set the base URL once here
